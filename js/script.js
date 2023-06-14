21d8ea835568299ff66913dc2b96b12dbf7ab144
Marcella Vizoná Vasconcelos <126676999+MarcellaVizona@users.noreply.github.com>
2023-06-14 14:37:24 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var fruta = "fruta";
+
+   fruta = prompt('Digite o nome de uma fruta');
+
+switch (fruta){
+  case "Maçã":
+    alert("Não vendemos esta fruta aqui");
+    break;
+  case "Kiwi":
+    alert("Estamos com escassez de kiwis");
+    break;
+  case "Melancia":
+    alert("Aqui está, são 3 reais o quilo");
+    break;
+    
+
+    default:
+        alert("Fruta Invalida")
+    
+}
